4bc866c344757b6b8ac7824882fe944085987a0b
giraud <giraud.contact@yahoo.fr>
2026-01-05 17:07:55 +0100

    Simplify config

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -7,12 +7,9 @@ vim.opt.termguicolors = true
 
 return {
   "nvim-tree/nvim-tree.lua",
-  version = "*",
   lazy = false,
   dependencies = {
     "nvim-tree/nvim-web-devicons",
   },
-  config = function()
-    require("nvim-tree").setup {}
-  end,
+  opts = { }
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,24 +4,10 @@ return {
       'nvim-lua/plenary.nvim',
       { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
     },
-    config = function()
-        require('telescope').setup({})
-
-        local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-        vim.keymap.set('n', '<leader>pws', function()
-            local word = vim.fn.expand("<cword>")
-            builtin.grep_string({ search = word })
-        end)
-        vim.keymap.set('n', '<leader>pWs', function()
-            local word = vim.fn.expand("<cWORD>")
-            builtin.grep_string({ search = word })
-        end)
-        vim.keymap.set('n', '<leader>ps', function()
-            builtin.grep_string({ search = vim.fn.input("Grep > ") })
-        end)
-        vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
-    end
+    keys = {
+      { '<leader>pf', require('telescope.builtin').find_files },
+      { '<leader>vh', require('telescope.builtin').help_tags },
+      { '<C-p>', require('telescope.builtin').git_files },
+    }
 }
 
